9ecd2dd725bae162638af260a219461117ac11cf
Frank Ngo and Hannah Lee <hannah@>
2018-04-02 16:11:57 -0700

    add style and hard coded in post for askreddit and worldnews

--- src/app/askreddit/askreddit.component.ts
+++ src/app/askreddit/askreddit.component.ts
@@ -12,9 +12,9 @@ export class AskredditComponent implements OnInit {
   constructor(private router: Router){}
 
     posts: Post[] = [
-   new Post("1 1/2 years later Our product doesn’t last that long! O_O", "felicianh",
-       "r/funny","https://a.thumbs.redditmedia.com/lQdhVLJTO4ehEcHqTDpOJac7tYpuaQ0hlfRLTArmoi0.jpg", 76.3,1),
-   new Post("Giant Squid makes an appearance in Tokyo Bay", " RyanSmith",
+   new Post("You are given 1 hour to communicate with your pets so that they understand you perfectly and you understand them perfectly. What do you guys talk about?", "YellowUmbrellaGuy",
+       "r/askreddit","https://a.thumbs.redditmedia.com/lQdhVLJTO4ehEcHqTDpOJac7tYpuaQ0hlfRLTArmoi0.jpg", 76.3,1),
+   new Post("What is your most terrifying animal encounter?", "  nightmaregirl18",
        "r/TheDepthsBelow","https://b.thumbs.redditmedia.com/ydCkUxZoKvEVr1ns600xdd-NOvUWg_Pt8sFJCOg4ehE.jpg",32.5,2),
    new Post("The year I peaked (Circa 1992)", "heChadillac",
        "r/OldSchooCool","https://b.thumbs.redditmedia.com/kg40YhofqEDqO3dRhgyU8Jafkit8AHRUMFl8e4wR41Y.jpg",20.5,3),
